c8c19bc62958c56d3b2b4439d6288ba987bfe99c
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2025-04-24 19:03:48 +0200

    bais

---
+++ src/recognition/gensettings.py
@@ -0,0 +1,101 @@
+def generate_coco_classes_file():
+    coco_classes = {
+        0: "person",
+        1: "bicycle",
+        2: "car",
+        3: "motorcycle",
+        4: "airplane",
+        5: "bus",
+        6: "train",
+        7: "truck",
+        8: "boat",
+        9: "traffic light",
+        10: "fire hydrant",
+        11: "stop sign",
+        12: "parking meter",
+        13: "bench",
+        14: "bird",
+        15: "cat",
+        16: "dog",
+        17: "horse",
+        18: "sheep",
+        19: "cow",
+        20: "elephant",
+        21: "bear",
+        22: "zebra",
+        23: "giraffe",
+        24: "backpack",
+        25: "umbrella",
+        26: "handbag",
+        27: "tie",
+        28: "suitcase",
+        29: "frisbee",
+        30: "skis",
+        31: "snowboard",
+        32: "sports ball",
+        33: "kite",
+        34: "baseball bat",
+        35: "baseball glove",
+        36: "skateboard",
+        37: "surfboard",
+        38: "tennis racket",
+        39: "bottle",
+        40: "wine glass",
+        41: "cup",
+        42: "fork",
+        43: "knife",
+        44: "spoon",
+        45: "bowl",
+        46: "banana",
+        47: "apple",
+        48: "sandwich",
+        49: "orange",
+        50: "broccoli",
+        51: "carrot",
+        52: "hot dog",
+        53: "pizza",
+        54: "donut",
+        55: "cake",
+        56: "chair",
+        57: "couch",
+        58: "potted plant",
+        59: "bed",
+        60: "dining table",
+        61: "toilet",
+        62: "tv",
+        63: "laptop",
+        64: "mouse",
+        65: "remote",
+        66: "keyboard",
+        67: "cell phone",
+        68: "microwave",
+        69: "oven",
+        70: "toaster",
+        71: "sink",
+        72: "refrigerator",
+        73: "book",
+        74: "clock",
+        75: "vase",
+        76: "scissors",
+        77: "teddy bear",
+        78: "hair drier",
+        79: "toothbrush"
+    }
+    file_content = ""
+    enhanced_classes = []
+    for class_id, class_name in coco_classes.items():
+        enhanced_classes.append({
+            "id": class_id,
+            "name": class_name,
+            "biasamount": 0.0  # Default bias amount set to 0.0
+        })
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+    file_content += pp.pformat(enhanced_classes)
+    output_file = "temp.py"
+    with open(output_file, "w") as f:
+        f.write(file_content)
+    print(f"Successfully generated '{output_file}' with {len(enhanced_classes)} COCO classes.")
+    return output_file
+if __name__ == "__main__":
+    generated_file = generate_coco_classes_file()
